45a8fd29c79c7dbc1a9bca4e33dfaa635c154e53
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-25 11:50:04 -0500

    Test that existing URLs from API are displayed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,9 +13,30 @@ describe('Home Page Initial Load', () => {
 
   it('Displays the form', () => {
     cy.get('form').within(() => {
-      cy.get('input[name=title]').should('exist').and('be.visible')
-      cy.get('input[name=urlToShorten]').should('exist').and('be.visible')
+      cy.get('input[name=title]')
+        .should('exist')
+        .and('be.visible')
+        .and('have.attr', 'Placeholder', 'Title...')
+      cy.get('input[name=urlToShorten]')
+        .should('exist')
+        .and('be.visible')
+        .and('have.attr', 'Placeholder', 'URL to Shorten...')
       cy.get('button').should('exist').and('be.visible')
     })
+  })
+
+  it('Displays any existing urls', () => {
+    cy.get('.urls-section').children().should('have.length', 1)
+    cy.get('div.url').find('h3').should('include.text', 'Awesome photo')
+    cy.get('div.url')
+      .find('a')
+      .should('include.text', 'http://localhost:3001/useshorturl/1')
+      .and('have.attr', 'href', 'http://localhost:3001/useshorturl/1')
+    cy.get('div.url')
+      .find('p')
+      .should(
+        'include.text',
+        'https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+      )
   })
 })
