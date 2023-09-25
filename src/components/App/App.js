9b382b92f29becb0590dd1c91e5bb6137174eaef
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-25 10:33:50 -0500

    Fetch data when App mounts

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { getUrls } from '../../apiCalls';
-import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { getUrls } from '../../apiCalls'
+import UrlContainer from '../UrlContainer/UrlContainer'
+import UrlForm from '../UrlForm/UrlForm'
 
-function App () {
-  const [urls, setUrls] = useState([]);
+function App() {
+  const [urls, setUrls] = useState([])
 
   useEffect(() => {
-
-  })
+    getUrls().then(data => setUrls(data.urls))
+  }, [])
 
   return (
-    <main className="App">
+    <main className='App'>
       <header>
         <h1>URL Shortener</h1>
         <UrlForm />
       </header>
 
-      <UrlContainer urls={"<<<Urls should go here>>>"}/>
+      <UrlContainer urls={urls} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
