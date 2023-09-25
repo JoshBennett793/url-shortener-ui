7c6434530d5ec391551d5f008d7f9b3d09956d02
Joshua Bennett <joshbennett793@yahoo.com>
2023-09-25 11:34:34 -0500

    Stub GET request and test for page title element

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Home Page Initial Load', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 200,
+      fixture: 'getRequest.json'
+    }).as('getRequest')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Displays page title', () => {
+    cy.get('header').find('h1').should('include.text', 'URL Shortener')
   })
 })
